7a7bb0bb3cf8f873783c2d472936c118d4c05873
timone019 <143154960+timone019@users.noreply.github.com>
2024-09-17 11:38:01 -0700

    Fix ALLOWED_HOSTS setting

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='localhost 127.0.0.1').split() + [ 'https://sleepy-island-60497-040803a19474.herokuapp.com/' ]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='localhost 127.0.0.1').split() + [ 'sleepy-island-60497-040803a19474.herokuapp.com' ]
 
 
 # Application definition
